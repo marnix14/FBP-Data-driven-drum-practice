7863b9a09a3b1612164cf541a28c017a41d21d65
Wijland <m.j.v.wijland@student.tue.nl>
2021-11-03 13:39:53 +0100

    ook met driehoekjes maar nog een beetje raar (kleine werken niet echt) (en ze zijn scheef)

--- stroke.js
+++ stroke.js
@@ -29,22 +29,25 @@ class Stroke {
             strokeWeight(sw);
             stroke(255);
             s = size;
-        
-            
+
+
 
         }
         else {
             noFill();
             strokeWeight(3);
             stroke(255, 255, 255, opacity);
-            s = size/2;
+            
         }
         switch (shape) {
             case 'circle':
-                ellipse(x + this.getDextSign() * (this.radius + spacing), y, this.radius * 2 * s , this.radius * 2 * s);
+                ellipse(x + this.getDextSign() * (this.radius + spacing), y, this.radius * 2 * s, this.radius * 2 * s);
                 break;
             case 'line':
                 line(x + this.getDextSign() * spacing, y, x + this.getDextSign() * (spacing + this.lineLength * s), y);
+                break;
+            case 'triangle':
+                triangle(x + this.getDextSign() * spacing, y, x + this.getDextSign() * (spacing + Math.sqrt(3) * this.lineLength * s), y + this.lineLength*s, x + this.getDextSign() * (spacing + Math.sqrt(3) * this.lineLength*s), y - s*this.lineLength);
                 break;
         }
     }
