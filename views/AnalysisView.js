1287738ffcc36cca1bd5c786e8078f124bf37dfb
Job Savelsberg <jobsavelsberg@gmail.com>
2021-11-09 21:12:11 +0100

    Draw linked hits relative to exercise hits

--- views/AnalysisView.js
+++ views/AnalysisView.js
@@ -39,20 +39,27 @@ class AnalysisView extends View {
 
     drawWrappedHitGraph() {
         for (const exerciseHit of this.exerciseHitAnalysis) {
-            this.drawHit(exerciseHit, "white", 5);
+            this.drawHit(exerciseHit.barPosition, exerciseHit.velocity, exerciseHit.getDexteritySign(), "white", 5);
 
             for (const recordedHit of exerciseHit.recordedHits) {
-                this.drawHit(recordedHit, "rgba(0,255,0,0.3)", 3);
+                this.drawLinkedHit(exerciseHit, recordedHit, "rgba(0,255,0,0.3)", 3);
             }
             for (const wrongHit of exerciseHit.wrongHits) {
-                this.drawHit(wrongHit, "rgba(255,0,0,0.3)", 3);
+                this.drawLinkedHit(exerciseHit, wrongHit, "rgba(255,0,0,0.3)", 3);
             }
         }
     }
 
-    drawHit(hit, color, weight = 2) {
-        const x = ((hit.barPosition / this.exerciseSession.exercise.bars) % 1) * width * 0.8;
-        const y = hit.getDexteritySign() * hit.velocity * 200;
+    drawLinkedHit(exerciseHit, recordedHit, color, weight) {
+        const barPosition =
+            exerciseHit.barPosition +
+            getWrappedOffset(exerciseHit.barPosition, recordedHit.barPosition, this.exerciseSession.exercise.bars);
+        this.drawHit(barPosition, recordedHit.velocity, recordedHit.getDexteritySign(), color, weight);
+    }
+
+    drawHit(barPosition, velocity, dexteritySign, color, weight = 2) {
+        const x = (barPosition / this.exerciseSession.exercise.bars) * width * 0.8;
+        const y = dexteritySign * velocity * 200;
         strokeWeight(weight);
         stroke(color);
         line(width * 0.1 + x, height / 2, width * 0.1 + x, height / 2 - y);
